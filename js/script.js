8b93b0d0772af2104b35e1fc6db33aebfd331c75
asliddin727 <asliddinmeyliyev1@gmail.com>
2023-09-27 23:42:00 +0500

    Completed work

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let telegram = document.querySelector('.alert');
+
+function open_message(){
+    telegram.style.display = 'block';
+}
+
+function close_message(){
+    telegram.style.display = 'none';
+}
